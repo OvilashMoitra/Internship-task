5b79a8124eb7c275f09d56516788c0d80239d6ad
Ovilash Moitra <ovilashmoitraofficial@gmai.com>
2022-08-05 06:43:58 +0600

    404 page creted

--- src/Page Container/NotFound/NotFound.js
+++ src/Page Container/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 const NotFound = () => {
     return (
-        <div>
-            <p>page not found</p>
+        <div className='flex flex-col items-center h-screen justify-center'>
+            <img className='w-40' src="https://media2.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif?cid=ecf05e4781tn0ms0d45bzbgcmgxdjpkdpr6b42k7tpt3jjj9&rid=giphy.gif&ct=g" alt="Page Not Found" />
+            <Link to={'/home'} className='btn-primary btn-sm w-40 h-10'>Home</Link>
         </div>
     );
 };
