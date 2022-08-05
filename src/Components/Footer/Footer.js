0168f5d4d497c27727dc81554d148881af69ec14
Ovilash Moitra <ovilashmoitraofficial@gmai.com>
2022-08-05 06:15:48 +0600

    navbar link and footer updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer class="footer footer-center p-4 bg-base-300 text-base-content">
             <div>
-                <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+                <p>Copyright © 2022 - All right reserved by Ovilash Moitra</p>
             </div>
         </footer>
     );
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <div class="navbar shadow-lg">
@@ -9,8 +9,7 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                 </div>
-                <a class="btn btn-ghost normal-case text-xl">RandomUser</a>
-
+                <Link to={'/home'}>RandomUser</Link>
             </div>
             <div class="navbar-center hidden lg:flex">
             </div>
